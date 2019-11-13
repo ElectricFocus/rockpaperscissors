63dfc7011f459b16c7f9207ffcaf294675d5cdb9
ElectricFocus <Bobtherook@gmail.com>
2019-11-13 23:20:49 +0000

    hi

--- js/script.js
+++ js/script.js
@@ -8,7 +8,44 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
+var humanscore=0;
+var computerscore=0;
+let choices= ["rock","paper","scissors"];
+let computer = Math.random();
+let human;
+
+if (computer === .3333){
+    console.log("rock");
+} else if (computer >.3333 && computer < .6666){
+    console.log("paper");
+} else (computer >.6666 && computer < .9999)
+
+
+
 $("button").click(function(){
-  let message = $("#input").val();
-  $("#userchoice").html(`<p> ${message} </p> `);
+ $("#result").html("#input");
+ console.log("hi");
+
+
+ 
+
+if (human === rock && computer === rock){
+    $("#result").html("tie");
+} else if (human === rock && computer === paper){
+    $("#result").html("computer wins");
+}else if (human === rock && computer === scissors){
+    $("#result").html("human wins");
+}else if (human === paper && computer === rock){
+    $("#result").html("tie");
+} else if (human === paper && computer === paper){
+    $("#result").html("computer wins");
+}else if (human === paper && computer === scissors){
+    $("#result").html("human wins");
+}else if (human === scissors && computer === rock){
+    $("#result").html("tie");
+} else if (human === scissors && computer === paper){
+    $("#result").html("computer wins");
+}else if (human === scissors && computer === scissors){
+    $("#result").html("human wins");
+}
 });
