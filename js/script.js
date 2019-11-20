c66bc7fd6d6fb3bb4d9a8afb3c5f149d158936e4
ElectricFocus <Bobtherook@gmail.com>
2019-11-20 22:27:35 +0000

    your message here

--- js/script.js
+++ js/script.js
@@ -13,39 +13,57 @@ var computerscore=0;
 let choices= ["rock","paper","scissors"];
 let computer = Math.random();
 let human;
-
-if (computer === .3333){
-    console.log("rock");
-} else if (computer >.3333 && computer < .6666){
-    console.log("paper");
-} else (computer >.6666 && computer < .9999)
-
-
+let cc;
 
 $("button").click(function(){
- $("#result").html("#input");
- console.log("hi");
+if (computer <= .3333){
+    cc = ("rock");
+    $("#computerChoice").html(choices[0]);
+} else if (computer >.3333 && computer < .6666){
+    cc = ("paper");
+    $("#computerChoice").html(choices[1]);
+} else if (computer >.6666 && computer < .9999){
+    cc = ("scissor");
+    $("#computerChoice").html(choices[2]);
+}
+
+
+
+let Uinput = $("#input").val().toLowerCase();
+
+ 
+ 
 
 
  
 
-if (human === rock && computer === rock){
+if ((Uinput) === ("rock") && cc === ("rock")){
     $("#result").html("tie");
-} else if (human === rock && computer === paper){
+    $("#userChoice").html("rock");
+} else if ((Uinput) === ("rock")&& cc === ("paper")){
     $("#result").html("computer wins");
-}else if (human === rock && computer === scissors){
+    $("#userChoice").html("rock");
+}else if ((Uinput) === ("rock") && cc === ("scissor")){
     $("#result").html("human wins");
-}else if (human === paper && computer === rock){
+    $("#userChoice").html("rock");
+}else if ((Uinput) === ("paper") && cc === ("rock")){
+    $("#result").html("human win");
+    $("#userChoice").html("paper");
+} else if ((Uinput) === ("paper") && cc === ("paper")){
     $("#result").html("tie");
-} else if (human === paper && computer === paper){
+    $("#userChoice").html("paper");
+}else if ((Uinput) === ("paper") && cc === ("scissor")){
+    $("#result").html("cumputer wins");
+    $("#userChoice").html("paper");
+}else if ((Uinput) === ("scissors") && cc === ("rock")){
     $("#result").html("computer wins");
-}else if (human === paper && computer === scissors){
+    $("#userChoice").html("scissors");
+} else if ((Uinput) === ("scissors") && cc === ("paper")){
     $("#result").html("human wins");
-}else if (human === scissors && computer === rock){
+    $("#userChoice").html("scissors");
+}else if ((Uinput) === ("scissors") && cc === ("scissor")){
     $("#result").html("tie");
-} else if (human === scissors && computer === paper){
-    $("#result").html("computer wins");
-}else if (human === scissors && computer === scissors){
-    $("#result").html("human wins");
+    $("#userChoice").html("scissors");
+
 }
 });
